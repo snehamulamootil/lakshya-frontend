b2bfad22af587226a86987cc7228eb287092b331
snehamulamootil <snehamulamootil@gmail.com>
2025-12-28 14:02:16 +0530

    UI responsiveness update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration:'enabled' })],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled',scrollPositionRestoration:'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
